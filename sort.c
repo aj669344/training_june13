bd2960e30ecebd6855a75d873aa6caad35ed9915
atishay <54525782+aj669344@users.noreply.github.com>
2024-06-26 09:47:46 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -21,3 +21,24 @@ int main(){
 
 
 }
+
+/*  
+   my code
+    int arr[]={5,4,3,2,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("unsorted array\n");
+    for(int i=0;i<n;i++) printf("%d ", arr[i]);
+    
+    for(int i=0;i<n-1;i++){       
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    
+    printf("\nsorted array\n");
+    for(int i=0;i<n;i++) printf("%d ", arr[i]);
+    */
